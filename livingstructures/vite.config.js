d5f36f216968cd41962b90eadbb776d348506b5d
Jaan Van Rompaey <tg9sz82jq5@privaterelay.appleid.com>
2026-06-20 12:24:56 +0200

    Update vite.config.js

--- livingstructures/vite.config.js
+++ livingstructures/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // Replace 'livingstructures' with your exact GitHub repository name
+  base: '/livingstructures/', 
+  plugins: [react()]
 })
