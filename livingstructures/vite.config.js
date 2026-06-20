55706df5db0c994b2c22662c70d4de8749d3633b
Jaan Van Rompaey <tg9sz82jq5@privaterelay.appleid.com>
2026-06-20 12:30:19 +0200

    Delete livingstructures directory

--- livingstructures/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  // Replace 'livingstructures' with your exact GitHub repository name
-  base: '/livingstructures/', 
-  plugins: [react()]
-})
